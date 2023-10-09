23bc6ce99f5055f2a7cac64963159fd77305c2e9
“simgeecnr” <csimge00@gmail.com>
2023-10-09 18:00:09 +0200

    Changing the test functions

--- tests/testthat/testing_func.R
+++ tests/testthat/testing_func.R
@@ -46,12 +46,12 @@ test_that("DataFrame has expected column names", {
 test_that("driver_winner() method works", {
   expect_output(driver_winner(2005), "Fernando Alonso won the most race which is 7 races with 133 points in 2005")
   expect_output(driver_winner(1972), "Emerson Fittipaldi won the most race which is 5 races with 61 points in 1972")
-  expect_output(driver_winner(2023), "Max Verstappen won the most race which is 13 races with 400 points in 2023")
+  expect_output(driver_winner(2022), "Max Verstappen won the most race which is 15 races with 454 points in 2022")
 })
 
 # 7) check the name of the driver who drive most between years a and b
 test_that("driver_winner_period() method works", {
-  expect_output(driver_winner_period(2020,2023), "Max Verstappen has the highest number of wins with 40 wins in total between years 2020 and 2023")
+  expect_output(driver_winner_period(2020,2022), "Max Verstappen has the highest number of wins with 27 wins in total between years 2020 and 2022")
   expect_output(driver_winner_period(1975, 1980), "Niki Lauda has the highest number of wins with 15 wins in total between years 1975 and 1980")
   expect_output(driver_winner_period(2015, 2018), "Lewis Hamilton has the highest number of wins with 40 wins in total between years 2015 and 2018")
 })
@@ -61,14 +61,14 @@ test_that("driver_winner_period() method works", {
 test_that("const_winner() method works", {
   expect_output(const_winner(2005), "McLaren constructer won the most race which is 10 races with 182 points in 2005")
   expect_output(const_winner(1972), "Team Lotus constructer won the most race which is 5 races with 61 points in 1972")
-  expect_output(const_winner(2023), "Red Bull constructer won the most race which is 15 races with 623 points in 2023")
+  expect_output(const_winner(2022), "Red Bull constructer won the most race which is 17 races with 759 points in 2022")
 })
 
 # 9) check the winner constructer for year interval
 test_that("const_winner_period() method works", {
   expect_output(const_winner_period(2010, 2015), "Red Bull constructor has the highest number of wins with 44 wins in total between years 2010 and 2015")
   expect_output(const_winner_period(1960, 1980), "Ferrari constructor has the highest number of wins with 50 wins in total between years 1960 and 1980")
-  expect_output(const_winner_period(2020, 2023), "Red Bull constructor has the highest number of wins with 45 wins in total between years 2020 and 2023")
+  expect_output(const_winner_period(2020, 2022), "Red Bull constructor has the highest number of wins with 30 wins in total between years 2020 and 2022")
 })
 
 # 10) 
